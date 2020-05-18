28ab589c33f3e555185848ec625e33349b28b321
Mike-Xie <Mike-Xie@users.noreply.github.com>
2020-05-18 16:13:15 -0700

    finish iterative binary search & mvp for monday

--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,32 @@
-def linear_search(arr, target):
+def linear_search(items: list, target: int):
     # Your code here
-
+    for i in items:
+    	if i == target:
+    		# important, this breaks if can't find 
+    		return items.index(target) 
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
-def binary_search(arr, target):
+def binary_search(items: list, target: int):
+    # does integer math, find item at halfway point
+    left = 0
+    right = len(items) - 1
 
-    # Your code here
+    while left <= right:
+    	# find midpoint
 
+    	mid = (left + right) // 2 
 
-    return -1  # not found
+    	if items[mid] == target:
+    		return mid
+
+    	if items[mid] < target:
+    		left = mid + 1
+
+    	else:
+    		right = mid - 1 
+
+    return -1 
+
